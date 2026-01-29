6f17b967f9c3adce2146d16f92460f36ecc82cf7
Lunar45-web <maximummoon3@gmail.com>
2026-01-29 09:09:55 +0300

    Update app.py

--- app.py
+++ app.py
@@ -178,4 +178,5 @@ if __name__ == '__main__':
     t.start()
     
     # Start the Dashboard (Blocking)
-    app.run_server(host='0.0.0.0', port=8050, debug=False)
+app.run(host='0.0.0.0', port=8050, debug=False)
+
